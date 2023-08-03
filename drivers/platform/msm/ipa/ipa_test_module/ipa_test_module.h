f7f5b047a1773ac0ec5defac1f647a72a2f799a7
Alin Jerpelea <alin.jerpelea@sony.com>
2023-08-03 10:54:10 +0200

    Revert "ipa: Support ipanat and kernel-tests compilation in LA"
    This reverts commit 11eaa09da23d9ad42539bab4d1576ee983631d9e.

--- drivers/platform/msm/ipa/ipa_test_module/ipa_test_module.h
+++ drivers/platform/msm/ipa/ipa_test_module/ipa_test_module.h
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note
+// SPDX-License-Identifier: GPL-2.0-only
 /*
 * Copyright (c) 2017-2018,2020-2021, The Linux Foundation. All rights reserved.
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 */
 
 #ifndef _IPA_TEST_MODULE_H_
